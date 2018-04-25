bd4a3e6257699350c7232586c8c0233d7d6f1ea1
Tao Cumplido <cumplido@rfmondial.de>
2018-04-25 20:13:55 +0200

    add zone.js 0.8.26 + router tests

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'help',
+    component: AppComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '/help',
+    pathMatch: 'full',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,49 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture, fakeAsync, tick } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
+import { routes } from './app-routing.module';
+
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let location: Location;
+  let router: Router;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule.withRoutes(routes)
       ],
       declarations: [
         AppComponent
       ],
     }).compileComponents();
   }));
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+
+  beforeEach(() => {
+    router = TestBed.get(Router);
+    location = TestBed.get(Location);
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+
+    router.initialNavigation();
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+  });
+
+  it('should create the app', async(() => {
+    expect(component).toBeTruthy();
   }));
+
+  it(`should redirect to '/help' for invalid routes (fakeAsync)`, fakeAsync(() => {
+    router.navigate(['foo']);
+    tick();
+    expect(location.path()).toEqual('/help');
+  }));
+
+  it(`should redirect to '/help' for invalid routes (async/await)`, async () => {
+    await router.navigate(['foo']);
+    expect(location.path()).toEqual('/help');
+  });
 });
